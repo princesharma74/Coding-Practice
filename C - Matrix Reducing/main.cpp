99cb45f72b83fc88c6d0f89042b6c227e36e253c
Prince Sharma <princesharma2899@gmail.com>
2022-08-21 13:43:35 +0530

    C

--- C - Matrix Reducing/main.cpp
+++ C - Matrix Reducing/main.cpp
@@ -39,6 +39,111 @@ int main(){
         B.push_back(vect); 
         cin.ignore(); 
     }
+
+    int ia = 0;
+    int ja = 0; 
+    bool bl = true; 
+
+    vector<int> del_row; 
+    vector<int> del_col; 
+
+    for(int i = 0; i < A.size(); i++){
+        ja = 0; 
+        for(int j = 0; j < A[i].size(); j++){
+            if(B[ia][ja] == A[i][j]){
+                ja++; 
+            }
+        }
+        if(ja == B[ia].size()){
+            ia++; 
+        }
+        else{
+            del_row.push_back(i);
+        }
+
+        if((A.size() - i) < (B.size() - ia)){
+            bl = false; 
+            break;
+        }
+
+    }
+
+    if(bl){
+
+    for(int i = 0; i < del_row.size(); i++){
+        A.erase(A.begin() + del_row[i] - i); 
+    }
+
+/*
+    for(int i = 0; i < A.size(); i++){
+        for(int j = 0; j < A[0].size(); j++){
+            cout << A[i][j] << " "; 
+        }
+        cout << endl; 
+    }
+*/
+
+    ia = 0; ja = 0; 
+    for(int j = 0; j < A[0].size(); j++){
+        ia = 0; 
+        for(int i = 0; i < A.size(); i++){
+            if(B[ia][ja] == A[i][j]){
+                ia++; 
+            }
+        }
+        if(ia == B.size()){
+            ja++; 
+        }
+        else{
+            del_col.push_back(j);
+        }
+
+        if((A[0].size() - j) < (B[0].size() - ja)){
+            bl = false; 
+            break;
+        }
+    }
     
+    if(bl){
+        cout << "Yes" << endl; 
+    }
+    else{
+        cout << "No" << endl; 
+    }
+
+    
+    for(int _i = 0; _i < A.size(); _i++){
+        for(int i = 0; i < del_col.size(); i++){
+            A[_i].erase(A[_i].begin() + del_col[i] - i);
+        } 
+    }
+
+    }
+    else{
+        cout << "No" << endl; 
+    }
+
+    for(int i = 0; i < A.size(); i++){
+        for(int j = 0; j < A[0].size(); j++){
+            cout << A[i][j] << " "; 
+        }
+        cout << endl; 
+    }
+/*
+    if(bl){ 
+            ia = 0; 
+            ja = 0; 
+            cout << A[0].size() << " - " << A.size(); 
+            for(int j = 0; j < A[0].size(); j++){
+                for(int i = 0; i < A.size(); i++){
+                    cout << A[i][j] << " "; 
+                }
+                cout << endl; 
+            }
+    }
+    else{
+        cout << "No" << endl; 
+    }
+*/
     return 0;
 }
