dd139e257d8860038a07f2a1795a9fecc57743d8
Prince Sharma <princesharma2899@gmail.com>
2022-10-13 01:26:24 +0530

    Oct 13

---
+++ B - i18n/main.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    string str; 
+    cin >> str; 
+    cout << str[0] << str.length()-2 << str[str.length()-1] << endl; 
+    return 0;
+}
